93aa9da738170abda3291143c29f1a38b99c5c6b
UrtziFM <urtzi.arana13@gmail.com>
2021-01-23 09:05:57 +0100

    Second exercise is finished

--- files.c
+++ files.c
@@ -36,16 +36,16 @@ char deleteBook(int referenceBook);
 char modifyBook(Book book); 
  
 
-int main() {
+int main() { //simple main function, we are using defined functions to interact with user requirements
     mainMenu();
     return 0;
 }
 
-void mainMenu() {
+void mainMenu() {  //this user interface
 
     int option;
  
-        do {
+        do {  //getting user selection 
     
             printf("\nMain Menu\n");
             printf("\n[1]. Show Library\n");
@@ -53,7 +53,7 @@ void mainMenu() {
             printf("\n[3]. Modify a Book\n");
             printf("\n[4]. Delete a Book\n");
             printf("\n[5]. Exit\n");
-            printf("\nInsert here your option: [ ]\n");
+            printf("\nInsert here your option: [ ]\n\n");
             scanf("%d", &option);
     
             switch (option) {
@@ -75,43 +75,43 @@ void mainMenu() {
                     break;
     
             } 
-        }while (option != 5);
+        }while (option != 5); // exit when option is 5
 }
 
-void showMenu() {
-	Book *books;
-	int totalBooks;
+void showMenu() { // we are getting books
+	Book *books; // it is a vector
+	int totalBooks; // we define a variable to know if there is any book in the vector 
  
-	books = getBook(&totalBooks); 
+	books = getBook(&totalBooks); //calling handling function to get books
  
 	if (totalBooks == 0) {
 		printf("\nThis file is empty\n");
 		getchar();
  
 	} else {
-		/* Se recorre el vector dinámico de productos */
-		for (int i = 0; i < totalBooks; i++) {
+		for (int i = 0; i < totalBooks; i++) { //showing each book information 
 			if (books[i].reference != -1) {
-				printf("%d %s %s %s \n", books[i].reference, books[i].title, books[i].writer, books[i].publishingHouse);
-                printf("%d", books[i].year);
+				printf("\nReference: %d \n\tISBN: %ld \tTitle:%s\n", books[i].reference, *books[i].ISBN, books[i].title);
+				printf("\tWriter:%s \tPublishingHouse:%s\n" , books[i].writer, books[i].publishingHouse);
+                printf("\tYear:%d \tPages:%d\n", *books[i].year, *books[i].pages);
 			}
 		}
 	}
 }
  
 
-void insertMenu(){
+void insertMenu(){  // we are introducing a book
 	Book book;
-	int referenceBook = 0;
+	int referenceBook = 0; // this is going to be the variable for book's reference 
  
             printf("\nBook Reference: ");
             scanf("%d", &referenceBook);
     
-            if (!validateBook(referenceBook, &book)) {
+            if (!validateBook(referenceBook, &book)) { // we are calling to handling function to validate if this reference already exist
     
-                book.reference = referenceBook;
+                book.reference = referenceBook; // we identify user reference with the structure
     
-                printf ("\nIntroduce Book's ISBN:\n");
+                printf ("\nIntroduce Book's ISBN [10 digits]:\n");
                     scanf("%ld", book.ISBN);
                 while(getchar() != '\n'); // cleaning entry buffer
                 printf ("\nIntroduce Book's Title:\n");
@@ -123,7 +123,7 @@ void insertMenu(){
                 printf ("\nIntroduce Book's Publishing House:\n");
                     fgets (book.publishingHouse, length, stdin);
                     book.publishingHouse[strcspn(book.publishingHouse, "\r\n")]= 0; // this function avoid line break of fgets
-                printf ("\nIntroduce Book's Year:\n");
+                printf ("\nIntroduce Book's Year [4 digits]:\n");
                     scanf("%d", book.year);
                 while(getchar() != '\n'); // cleaning entry buffer
                 printf ("\nIntroduce Book's pages:\n");
@@ -141,15 +141,15 @@ void insertMenu(){
             }
 }
 
-void deleteMenu() {
+void deleteMenu() {  // It wasn't in the exercise requirements but it is helpful to manage a Library
 	Book book;
 	int referenceBook;
  
 		printf("\nBook Reference: ");
 		scanf("%d", &referenceBook);
  
-		if (validateBook(referenceBook, &book)) {
-            if(deleteBook(referenceBook)){
+		if (validateBook(referenceBook, &book)) {  // we are calling to handling function to validate if this reference already exist
+            if(deleteBook(referenceBook)){  // we are calling to handling function to delete a book
                 printf("\nThis reference book has been deleted: %d\n", referenceBook);
             } else {
 			    printf("\nThis reference book it can't be deleted\n");
@@ -166,9 +166,9 @@ void modifyMenu() {
 		printf("\nCódigo de producto: ");
 		scanf("%d", &referenceBook);
  
-		if (validateBook(referenceBook, &book)) {
+		if (validateBook(referenceBook, &book)) { // we are calling to handling function to validate if this reference already exist
  
-            printf("\nInsert here new ISBN \n");
+            printf("\nInsert here new ISBN or old again\n");
             scanf("%ld", book.ISBN);
             while(getchar() != '\n'); // cleaning entry buffer
 			if (modifyBook(book)) {
@@ -176,7 +176,7 @@ void modifyMenu() {
 				} else {
                     printf("Impossible to modify");
                 }
-			printf("\nInsert here new title\n");
+			printf("\nInsert here new title or old again\n");
             fgets (book.title, length, stdin);
             book.title[strcspn(book.title, "\r\n")]= 0; // this function avoid line break of fgets
 			if (modifyBook(book)) {
@@ -184,7 +184,7 @@ void modifyMenu() {
 				} else {
                     printf("Impossible to modify");
                 }
-            printf("\nInsert here new writer\n");
+            printf("\nInsert here new writer or old again\n");
             fgets (book.writer, length, stdin);
             book.writer[strcspn(book.writer, "\r\n")]= 0; // this function avoid line break of fgets
 			if (modifyBook(book)) {
@@ -192,7 +192,7 @@ void modifyMenu() {
 				} else {
                     printf("Impossible to modify");
                 }
-            printf("\nInsert here new publishing house\n");
+            printf("\nInsert here new publishing house or old again\n");
             fgets (book.publishingHouse, length, stdin);
             book.publishingHouse[strcspn(book.publishingHouse, "\r\n")]= 0; // this function avoid line break of fgets
 			if (modifyBook(book)) {
@@ -200,7 +200,7 @@ void modifyMenu() {
 				} else {
                     printf("Impossible to modify");
                 }
-            printf("\nInsert here new year\n");
+            printf("\nInsert here new year or old again\n");
             scanf("%d", book.year);
             while(getchar() != '\n'); // cleaning entry buffer
 			if (modifyBook(book)) {
@@ -208,7 +208,7 @@ void modifyMenu() {
 				} else {
                     printf("Impossible to modify");
                 }
-            printf("\nInsert here new pages \n");
+            printf("\nInsert here new pages or old again \n");
             scanf("%d", book.pages);
             while(getchar() != '\n'); // cleaning entry buffer
 			if (modifyBook(book)) {
@@ -223,58 +223,57 @@ void modifyMenu() {
 	
  
 
-char validateBook(int referenceBook, Book *book) {
+char validateBook(int referenceBook, Book *book) { // defining handling function to validate if this reference already exist
 	FILE *file;
 	char exist;
  
-	file = fopen("books.txt", "rb");
+	file = fopen("books.txt", "rb"); //opening file
  
-	if (file == NULL) { 
+	if (file == NULL) { //handling if the file exist or not
 		exist = 0;
 	} else {
 		exist = 0;
  
-		fread(&(*book), sizeof(*book), 1, file);
+		fread(&(*book), sizeof(*book), 1, file); //reading file
 		while (!feof(file)) {
-			if ((*book).reference == referenceBook) {
+			if ((*book).reference == referenceBook) { // here is where we are validating if the book exist or not
 				exist = 1;
 				break;
 			}
 			fread(&(*book), sizeof(*book), 1, file);
 		}
- 		fclose(file);
+ 		fclose(file); // always we have to close file if we have opened it, better to avoid mistakes
 	}
-	return exist;
+	return exist; // now we know if this reference exist or not
 }
  
-char insertBook(Book book) {
+char insertBook(Book book) {  // defining handling function to insert a book
 	FILE *file;
 	char insert;
  
-	file = fopen("books.txt", "ab");	
+	file = fopen("books.txt", "ab"); //opening file
  
-	if (file == NULL) { 
-		insert = 0;
+	if (file == NULL) { // if the file doesn't exist we can insert anything, we can't open
+		insert = 0; 
  
 	} else {
-		fwrite(&book, sizeof(book), 1, file);
+		fwrite(&book, sizeof(book), 1, file); // writing file 
 		insert = 1;
 
-		fclose(file);
+		fclose(file); // always we have to close file if we have opened it, better to avoid mistakes
 	}
  
 	return insert;
 }
 
-char deleteBook(int referenceBook) {
+char deleteBook(int referenceBook) { // defining handling function to delete a book
 	FILE *file;
-	FILE *aux;
 	Book book;
 	char del;
  
 	file = fopen("books.txt", "r+b");	
  
-	if (file == NULL) { 
+	if (file == NULL) { //handling if the file exist or not
 		del = 0;
  
 	} else {
@@ -282,7 +281,7 @@ char deleteBook(int referenceBook) {
 		fread(&book, sizeof(book), 1, file);
 		while (!feof(file)) {
 			if (book.reference == referenceBook) {
-				fseek(file, ftell(file) - sizeof(book), SEEK_SET);
+				fseek(file, ftell(file) - sizeof(book), SEEK_SET); // we are getting first pointer in the file and the position of it
 				book.reference = -1;
 				fwrite(&book, sizeof(book), 1, file);
 				del = 1;
@@ -291,20 +290,20 @@ char deleteBook(int referenceBook) {
 			fread(&book, sizeof(book), 1, file);
 		}
 
-		fclose(file);
+		fclose(file); // always we have to close file if we have opened it, better to avoid mistakes
 	}
  
 	return del;
 }
 
-char modifyBook(Book book) {
+char modifyBook(Book book) { // defining handling function to delete a book
 	FILE *file;
-    Book booky;
+    Book booky; // defining the modified book 
 	char modify;
  
 	file = fopen("books.txt", "rb+");
  
-	if (file == NULL) { 
+	if (file == NULL) { //handling if the file exist or not
 		modify = 0;
  
 	} else {
@@ -312,7 +311,7 @@ char modifyBook(Book book) {
 		fread(&booky, sizeof(booky), 1, file);
 		while (!feof(file)) {
 			if (booky.reference == book.reference) {
-				fseek(file, ftell(file) - sizeof(book), SEEK_SET);
+				fseek(file, ftell(file) - sizeof(book), SEEK_SET); // we are getting first pointer in the file and the position of it
 				fwrite(&book, sizeof(book), 1, file);
 				modify = 1;
 				break;
@@ -326,7 +325,7 @@ char modifyBook(Book book) {
 	return modify;
 }
 
-Book *getBook(int *n) {
+Book *getBook(int *n) { //getting a book from the vector
 	FILE *file;
 	Book book;
 	Book *books; 
@@ -334,17 +333,17 @@ Book *getBook(int *n) {
  
 	file = fopen("books.txt", "rb");
  
-	if (file == NULL) { 
+	if (file == NULL) { //handling if the file exist or not
 		*n = 0; 
 		books = NULL;
  
 	} else {
  
-		fseek(file, 0, SEEK_END); 
+		fseek(file, 0, SEEK_END); // we are getting first pointer in the file and the position of it
 		*n = ftell(file) / sizeof(Book); 
 		books = (Book *)malloc((*n) * sizeof(Book)); 
  
-		fseek(file, 0, SEEK_SET); 
+		fseek(file, 0, SEEK_SET); // we are getting first pointer in the file and the position of it
 		fread(&book, sizeof(book), 1, file);
 		i = 0;
 		while (!feof(file)) {
